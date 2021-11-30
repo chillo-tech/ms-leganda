f997bccb9192f34924220844e3e271fa7cd23765
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2021-11-30 08:36:28 +0100

    deploiement

--- src/main/java/com/cs/ganda/MsLegandaApplication.java
+++ src/main/java/com/cs/ganda/MsLegandaApplication.java
@@ -3,7 +3,11 @@ package com.cs.ganda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableAsync
 @EnableFeignClients
 @SpringBootApplication(exclude = {
         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
